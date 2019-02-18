c282cb09667aabad921dbcce36894a4ef0990a62
Rodrigo Almeida Bonotto <rdgbtto@gmail.com>
2019-02-17 22:10:23 -0300

    Projeto Iniciado

---
+++ OrderClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
