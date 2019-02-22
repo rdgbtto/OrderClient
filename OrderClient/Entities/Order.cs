a1049db6158a42469364079c049f0b87bb0193e0
Rodrigo Almeida Bonotto <rdgbtto@gmail.com>
2019-02-21 21:01:49 -0300

    Criada a classe Order com suas Propriedades, Contrutores e Metodos

---
+++ OrderClient/Entities/Order.cs
@@ -0,0 +1,46 @@
+﻿using OrderClient.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderClient.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0;
+            foreach (OrderItem item in Items)
+            {
+                total += item.SubTotal();
+            }
+            return total;
+        }
+    }
+}
